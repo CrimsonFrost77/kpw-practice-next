b224246628cfb9bd7720cbf31e94591635768c90
shaheena <shaheenaebadi@northsouth.edu>
2025-01-01 01:56:47 +0600

    1st commit by ebadi

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+
+
+import MunicipalityDashboard from '@/components/MunicipalityDashboard';
+
+export default function about() {
+  return (
+    <div>
+      <MunicipalityDashboard />
+    </div>
+  );
+}
